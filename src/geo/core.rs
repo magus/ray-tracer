6310f3a615f6b24deee136de224f30bb843bfa0b
magus <noah@iamnoah.com>
2025-03-02 14:57:17 -0800

    random_unit_with_transform to implement random unit + random disk

--- src/geo/core.rs
+++ src/geo/core.rs
@@ -17,12 +17,31 @@ pub fn random_unit_normal_direction(normal: &Vec3) -> Vec3 {
 }
 
 pub fn random_unit() -> Vec3 {
+    random_unit_with_transform(|p| p)
+}
+
+pub fn random_unit_disk() -> Vec3 {
+    random_unit_with_transform(|mut p| {
+        p.z = 0.0;
+        p
+    })
+}
+
+fn random_unit_with_transform<T>(transform: T) -> Vec3
+where
+    T: Fn(Vec3) -> Vec3,
+{
     // rejection sample vector until it falls inside the unit
     loop {
         let p = Vec3::random_range(-1.0, 1.0);
+
+        let p = transform(p);
+
         let lensq = p.length_squared();
+
         if lensq <= 1.0 {
             let sqrtlensq = lensq.sqrt();
+
             // avoid potential division by zero for small values
             // e.g. 1e-160
             if sqrtlensq > 0.0 {
